c0e4cb5249ae531625b6bb8f0f2d1c6f8735a2d0
18016080 <18016080@myrp.edu.sg>
2019-05-24 16:10:54 +0800

    Draft

--- app/src/main/java/sg/edu/rp/soi/p06_quiz/MainActivity.java
+++ app/src/main/java/sg/edu/rp/soi/p06_quiz/MainActivity.java
@@ -1,17 +1,62 @@
 package sg.edu.rp.soi.p06_quiz;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
-
-
+EditText num1;
+EditText num2;
+TextView operation;
+Button reset;
+TextView ans;
+Button btns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        num1 = findViewById(R.id.etNum1);
+        num2 = findViewById(R.id.etNum2);
+        operation = findViewById(R.id.tvOperation);
+        reset = findViewById(R.id.btnReset);
+        ans = findViewById(R.id.results);
+        registerForContextMenu(operation);
+
+
     }
 
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        menu.add(0, 0, 0, "+");
+        menu.add(0, 1, 1, "-");
+
+    }
+    public boolean onContextItemSelected(MenuItem item) {
+        if(item.getItemId()==0) {
+            int i = Integer.parseInt(num1.getText().toString());
+            int s = Integer.parseInt(num2.getText().toString());
+            int total=i-s;
+            ans.setText(total);
+            return true;
+        }
+        else if(item.getItemId()==1) {
+            int i = Integer.parseInt(num1.getText().toString());
+            int s = Integer.parseInt(num2.getText().toString());
+            int total=i+s;
+            ans.setText(total);
+            return true;
+        }
+        return super.onContextItemSelected(item);
+    }
 
 }
